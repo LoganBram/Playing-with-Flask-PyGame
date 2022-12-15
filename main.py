40eee441a9d7c57b8823e63b72f3a366b8704d54
Logan Bramwell <loganbramwellqu@gmail.com>
2022-12-15 16:55:18 -0500

    base of game, checking for quit, make sure window doesnt close after running

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pygame
 
-pygame.init()
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def main():
+    # inf loop so game display doesnt close
+    run = True
+    while run:
+        # getting list of all events, loop through them to check them
+        for event in pygame.event.get():
+            # check if user closed window
+            if event.type == pygame.QUIT:
+                run = False
+        pygame.quit()
+
+
+main()
